c83dba4d5cafde33ea9a6ed86e3dc862275e9fa3
Arnav-Kudiyal <reacharnavkudiyal@gmail.com>
2025-08-18 18:24:42 +0530

    for_loop_problem.cpp

---
+++ basics/for_loop_problem.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a,b,i;
+    string number[10]={"one","two","three","four","five","six","seven","eight","nine"};
+    cin>>a>>b;
+    
+    for(i=a;i<=b;i++)
+    {
+        if (i>=1 && i<=9)
+        {
+        cout<<number[i-1]<<endl;
+        }
+        else if (i%2==0)
+        {
+        cout<<"even"<<endl;
+        }
+        else 
+        {
+        cout<<"odd"<<endl;
+        }
+    
+    }
+    return 0;
+}
