6ab29813db211c1a100a471e049132c1f2622222
Arnav Kudiyal <reacharnavkudiyal@gmail.com>
2025-11-17 20:57:30 +0530

    Create PracticeProblem12.cpp

---
+++ basics/PracticeProblem12.cpp
@@ -0,0 +1,29 @@
+// Program to find maximum element in an array.
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int digit = 0;
+    int list[10];
+
+    cout << " Enter the numbers in the array" << endl;
+
+    for(int i = 0; i < 10; i++)
+    {
+        cin >> list[i];
+    }
+
+    for(int i = 0; i < 10; i++)
+    {
+        if (list[i] > digit)
+        {
+            digit = list[i];
+        }
+        
+    }
+
+    cout << "The maximum number in the array is " << digit;
+
+    return 0;
+}
